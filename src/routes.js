11cb0970fff22abf8b1b8fad04d8f97421b944ec
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-10 17:43:26 +0000

    installed express validator, and created validator for signup

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Auth = require('../src/middlewares/Auth');
-
+const AuthValidator = require('./validators/AuthValidator');
 
 const AuthController = require('./controllers/AuthController')
 const UserController = require('./controllers/UserController')
@@ -15,7 +15,7 @@ router.get('/ping', (req, res) => {
 router.get('/states',  UserController.getStates);
 
 router.post('/user/signin', AuthController.signin);
-router.post('/user/signup', AuthController.signup);
+router.post('/user/signup', AuthValidator.signup,AuthController.signup);
 
 router.get('/user/me', Auth.private, UserController.info);
 router.put('/user/me', Auth.private,UserController.editAction);
---
+++ src/validators/AuthValidator.js
@@ -0,0 +1,32 @@
+const { checkSchema } = require('express-validator');
+
+module.exports = {
+
+    signup:checkSchema({
+        name: {
+            trim: true,
+            notEmpty: true,
+            isLength:{
+                options: { min: 2}
+            },
+            errorMessage: 'O Nome precisa ter pelo menos 2 caracteres'
+        },
+        email: { 
+            isEmail: true,
+            normalizeEmail: true,
+            errorMessage: 'E-mail invalido'
+        },
+        password: { 
+            isLength: { 
+
+                options: { min: 2}
+
+            },
+            errorMessage: 'Senha precisa ter pelo menos 2 caracteres'
+        },
+        state: { 
+            notEmpty: true,
+            errorMessage: 'Estado nao preenchido'
+        }
+    })
+}
