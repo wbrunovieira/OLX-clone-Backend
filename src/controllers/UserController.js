ee04f1ee5b8f926a64003635575cc6f00aa968ba
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-12 00:46:25 +0000

    added list user with ads

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,7 @@
 const State = require('../models/State');
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Ad = require('../models/Ad');
 
 module.exports = {
 
@@ -7,6 +10,27 @@ getStates: async(req,res) => {
     res.json({states});
 },
 info: async(req, res) => {
+    let token = req.query.token;
+    
+    const user = await User.findOne({token});
+    const state = await State.findById(user.state);
+    const ads = await Ad.find({idUser: user._id.toString()});
+
+    let adList = [];
+    for(let i in ads){
+        const cat = await Category.findById(ads[i].category);
+        adList.push({
+            ...ads[i], category: cat.slug
+        });
+
+    }
+
+    res.json({
+        name: user.name,
+        email: user.email,
+        state: state.name,
+        ads: adList
+    });
 
 },
 editAction: async(req, res) => {
